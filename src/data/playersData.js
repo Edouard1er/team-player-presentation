4a4827206cb5c743f12040e0515f34396394fa4e
Edouard Chevenslove <20221763@webmail.universita.corsica>
2023-12-19 14:25:54 +0100

    Update playersData.js

--- src/data/playersData.js
+++ src/data/playersData.js
@@ -45,7 +45,7 @@ const playersData = [
       <div>
         <p>Meet our It's difficuuuuullllt, Idricealy MOURTADHOI or Mustard, who at the tender age of 22 (born on 29/04/2001) is already conquering the coding world or not haha.</p>
         <p>Child of the beautiful city of Marseille, France, Idricealy possesses coding strengths in Kotlin and Vue.js, making others green with envy except Eddy.</p>
-        <p>But beware! His one weakness, which he boldly admits, is the mysterious realm of artificial intelligence. Yet, in a surprising twist, he proudly claims his biggest achievement in coding to be the victorious Hackathon of 2022, dedicated to FPF. Oh, the irony!</p>
+        <p>But beware! His one weakness, which he boldly admits haha, is the mysterious realm of artificial intelligence. Yet, in a surprising twist, he proudly claims his biggest achievement in coding to be the victorious Hackathon of 2022, dedicated to FPF. Oh, the irony!</p>
       </div>
     ,
   },
@@ -59,15 +59,15 @@ const playersData = [
     codingWeakness: 'IA',
     biggestAchievementInCoding: 'A Monitoring and Evaluation Platform for a consortium (ACF, HEIFER, JWT)',
     interests: 'Football and Music',
-    position: 'Defender', // Vous pouvez ajouter une position spécifique si nécessaire
-    avatar: Eddy2, // Assurez-vous que Pilou est correctement importé
-    avatar2: Eddy1, // Assurez-vous que Pilou est correctement importé
+    position: 'Defender', 
+    avatar: Eddy2, 
+    avatar2: Eddy1, 
     detailText: (
       <div>
-        <p>Meet the amazing coding guru, Chevenslove EDOUARD, who at the age of 25 (born on 06/02/1998) is making waves in the coding world.</p>
-        <p>Hailing from the beautiful town of Leogane, Haiti, Chevenslove excels in JavaScript and React, leaving others in awe of his skills.</p>
-        <p>However, even coding maestros have their kryptonite, and for Chevenslove, it's the enigmatic world of artificial intelligence. Despite this, he proudly boasts his greatest coding achievement: developing a Monitoring and Evaluation Platform for a consortium comprising ACF, HEIFER, and JWT. Bravo!</p>
-        <p>When he's not crafting code, Chevenslove indulges in his passions for football and music. A man of many talents!</p>
+        <p>Meet the amazing guy, the angel, the love for the entire world, the shining man, Chevenslove EDOUARD, AKA Eddy, who at the age of 25 (born on 06/02/1998) is making waves in the coding world.</p>
+        <p>Eddy is from the town of Leogane, a treasure city in Haiti. Eddy excels in JavaScript and React. And that's why he uses React to develop this website, Olala, he's not humble haha.</p>
+        <p>However, even coding superman have their kryptonite, and for Chevenslove, it's the enigmatic world of artificial intelligence. Despite this, he proudly boasts his greatest coding achievement: developing a Monitoring and Evaluation Platform for a consortium comprising ACF, HEIFER, and JWT. Please clap your hands for Eddy!</p>
+        <p>When he's not crafting code, Chevenslove indulges in his passions for football and music, mainly haitian music named Konpa. A man of many talents and many surprises!</p>
       </div>
     ),
   },
@@ -80,14 +80,14 @@ const playersData = [
     codingWeakness: 'IA',
     biggestAchievementInCoding: 'No Code',
     interests: 'Central Back in Football',
-    position: 'Forwad', // Ajoutez une position spécifique si nécessaire
-    avatar: Pilou1, // Assurez-vous que Pilou est correctement importé
-    avatar2: Pilou2, // Assurez-vous que Pilou est correctement importé
+    position: 'Forwad', 
+    avatar: Pilou1, 
+    avatar2: Pilou2, 
     detailText: (
       <div>
-        <p>Meet the extraordinary coder, Pierre-Louis SQUAGLIA, who, at the age of 22 (born on 14/11/2001), has left an indelible mark on the coding landscape.</p>
-        <p>Originally hailing from Bastia, Corse, Pierre-Louis demonstrates unparalleled coding prowess in JavaScript and Angular, establishing himself as a force to be reckoned with.</p>
-        <p>While his coding forte lies in the world of web technologies, he openly admits to being somewhat mystified by the intricacies of artificial intelligence. But, here's the twist: his most significant achievement in coding is proudly declaring "No Code." A true renegade!</p>
+        <p>What if we introduce you the extraordinary coder, Pierre-Louis SQUAGLIA ?? He is very young, he is 22 years old but he has already left an indelible mark on the coding landscape.</p>
+        <p>Pilou is a Bastian, a Corsican. Pilou demonstrates unparalleled coding prowess in JavaScript and Angular, establishing himself as a force to be reckoned with.</p>
+        <p>While his coding forte lies in the world of web technologies, even though it was difficuuuuullllt, he openly admited to being somewhat mystified by the intricacies of artificial intelligence. But, here's the twist: his most significant achievement in coding is proudly declaring "No Code." A true renegade!</p>
         <p>Off the keyboard, Pierre-Louis finds solace as a Central Back in football, where he exhibits strategic brilliance much like his coding exploits.</p>
       </div>
     ),
@@ -106,7 +106,7 @@ const playersData = [
     avatar2: Toussaint2, 
     detailText: (
       <div>
-        <p>Meet the coding maestro, Toussaint CARLOTTI, who, at the age of 23 (born on 17/12/2000), has carved a niche in the coding world.</p>
+        <p>Toussaint Toussaint, no we he is not Toussaint LOUVERTURE, the Haitian Hero, but he is Toussaint CARLOTTI, who, has carved a niche in the coding world. A hero on his way !!!</p>
         <p>Originally from Ajaccio, Corse, Toussaint exhibits exceptional coding skills in Laravel and VueJs, establishing himself as a formidable force in the tech arena.</p>
         <p>While he excels in various coding domains, he candidly admits to his proficiency as a web developer, showcasing his versatility in the field.</p>
         <p>Toussaint's most notable coding achievement is the creation of an application designed to manage a company's quotes and customers, a testament to his prowess in developing practical solutions.</p>
@@ -123,12 +123,12 @@ const playersData = [
     biggestAchievementInCoding: 'Quoted in Corse-matin but not for coding',
     interests: 'FootBall, culture and money',
     position: 'Defender',
-    avatar: Dimitri2, // Assurez-vous que Pilou est correctement importé
-    avatar2: Dimitri1, // Assurez-vous que Pilou est correctement importé
+    avatar: Dimitri2, 
+    avatar2: Dimitri1, 
     detailText: (
       <div>
-        <p>Meet the enigmatic coder, Dimitri Denis, aged 25 (born on 21/05/1998), who adds a unique flair to the coding community.</p>
-        <p>Hailing from Tours, France, Dimitri specializes in the intricate world of Angular, showcasing his expertise in this sophisticated framework.</p>
+        <p>Dimitri Denis, the Red Bull man haha. Not only good in drinking bad drinks, he adds a unique flair to the coding community.</p>
+        <p>Didy is from Tours, France. He specializes in the intricate world of Angular, showcasing his expertise in this sophisticated framework.</p>
         <p>Surprisingly, his coding journey is not without its quirks—Dimitri candidly declares that his coding weaknesses encompass "Everything else," making him a true maverick in the field.</p>
         <p>Despite his coding prowess, Dimitri's biggest achievement isn't related to coding at all. He earned recognition in Corse-matin, not for his coding feats, but for something entirely different.</p>
         <p>When not immersed in the coding realm, Dimitri's interests span football, culture, and the pursuit of financial success.</p>
@@ -150,8 +150,8 @@ const playersData = [
     avatar2: Felicien2, 
     detailText: (
       <div>
-        <p>Meet the coding virtuoso, Félicien BERTRAND, aged 22 (born on 14/07/2001), whose skills elevate him in the realm of programming even if he is shy haha.</p>
-        <p>Originally from Bastia, Corsica, Félicien exhibits mastery in Python and database technologies, solidifying his position as a coding maestro.</p>
+        <p>Is this Jean BERTRAND or Félicien BERTRAND ??? it's difficuuuuullllt to say... So we go with Félicien BERTRAND, aged 22 (born on 14/07/2001), whose skills elevate him in the realm of programming even if he is very shy haha.</p>
+        <p>Native of Bastia, the team in which plays my favory haitian Jhonny Placide, the goalkeeper. Let's go back to Félicien.... He is extremely good in Python and database technologies, solidifying his position as a coding maestro.</p>
         <p>Despite his proficiency, Félicien humbly acknowledges that his coding journey encounters a roadblock with artificial intelligence, showcasing his dedication to continuous improvement.</p>
         <p>Notably, Félicien's most significant achievement in coding is securing victory in the PIX Challenge Innovation 2023, marking a milestone in his coding career.</p>
         <p>Beyond coding, Félicien's interests extend to a wide spectrum, encompassing all sports and indulging in the virtual realms of video games.</p>
